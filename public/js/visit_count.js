5c895e3aaa9e890041d14855a52af217e456dcca
Justin Thibault <tbojustin@users.noreply.github.com>
2021-11-26 18:59:32 -0500

    Update visit_count.js
    Fix misspelling

--- public/js/visit_count.js
+++ public/js/visit_count.js
@@ -1,6 +1,6 @@
 function visit_count() {
     fetch("https://w894g5a5sa.execute-api.us-east-1.amazonaws.com/test")
         .then(response => response.json())
-        .then(data => document.getElementById('Visit_Count').innerHTML = `Vists: ${data}`)
+        .then(data => document.getElementById('Visit_Count').innerHTML = `Visits: ${data}`)
         .catch(err => console.log(err));
 }
