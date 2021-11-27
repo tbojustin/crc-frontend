fb8212c1079c2e789e9c3c2e408605b705ece106
tbojustin <tbojustin@users.noreply.github.com>
2021-11-27 09:05:03 -0500

    Changed API Name

--- public/js/visit_count.js
+++ public/js/visit_count.js
@@ -1,5 +1,5 @@
 function visit_count() {
-    fetch("https://w894g5a5sa.execute-api.us-east-1.amazonaws.com/test")
+    fetch("https://isi03rijgg.execute-api.us-east-1.amazonaws.com/test")
         .then(response => response.json())
         .then(data => document.getElementById('Visit_Count').innerHTML = `Visits: ${data}`)
         .catch(err => console.log(err));
